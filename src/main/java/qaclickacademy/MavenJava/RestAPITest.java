691dd4b6f71992d220e160b3ef7c23ac97956b28
rn <rntester5@gmail.com>
2020-08-13 16:49:55 -0400

    American dued

--- src/main/java/qaclickacademy/MavenJava/RestAPITest.java
+++ src/main/java/qaclickacademy/MavenJava/RestAPITest.java
@@ -10,7 +10,14 @@ public class RestAPITest {
 		System.out.println("postJira2");
 		System.out.println("postJira3");
 		System.out.println("postJira4");
+
+	//additional moifications 
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
+		System.out.println("postJira8");
 	}
+	
 	@Test
 	public void deleteTwitter(){
 		System.out.println("deleteTwitter");
